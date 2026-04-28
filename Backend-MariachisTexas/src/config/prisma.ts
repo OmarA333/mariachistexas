4890fefce8f3878e3c8e19010c8acd6b41e4422a
OmarA333 <gonzalez.andres890@gmail.com>
2026-04-28 01:25:49 -0500

    prueba7

--- Backend-MariachisTexas/src/config/prisma.ts
+++ Backend-MariachisTexas/src/config/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma'
+import { PrismaClient } from '../generated/prisma/client'
 import 'dotenv/config'
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient()
 
 export default prisma
 
