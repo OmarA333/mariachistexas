69f9a8dcd87ec9350c63f002de2eeb2bf4f727a3
OmarA333 <gonzalez.andres890@gmail.com>
2026-04-28 03:20:13 -0500

    prueba9

--- Backend-MariachisTexas/prisma.config.ts
+++ Backend-MariachisTexas/prisma.config.ts
@@ -1,11 +1,7 @@
-import path from 'node:path'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { defineConfig } from 'prisma/config'
-import 'dotenv/config'
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: path.join('prisma', 'schema.prisma'),
-  datasource: {
-    url: process.env.DATABASE_URL,
+  migrations: {
+    seed: 'npx ts-node prisma/seed.ts',
   },
-})
+});
