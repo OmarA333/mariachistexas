609d1105faf8484e45a905bd1e90a3c50417a3fe
OmarA333 <gonzalez.andres890@gmail.com>
2026-05-07 00:43:38 -0500

    listo2

--- Backend-MariachisTexas/src/config/mailer.ts
+++ Backend-MariachisTexas/src/config/mailer.ts
@@ -2,14 +2,16 @@ import nodemailer from 'nodemailer'
 import 'dotenv/config'
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
+  host:   process.env.MAIL_HOST   || 'smtp.gmail.com',
+  port:   Number(process.env.MAIL_PORT) || 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
-});
-
+  tls: {
+    rejectUnauthorized: false,
+  },
+})
 
 export default transporter
